61ca0daef5431eed4e29983da6a9d6c8cd16eb80
Luke <flaherty.luke@yahoo.com>
2023-08-09 22:56:21 -0400

    smaller file

--- mergeStringsAlternatively.py
+++ mergeStringsAlternatively.py
@@ -1,19 +1,14 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        
         result = ""
         i = 0
-
-        while len(word1) > i or len(word2) > i:
-            if len(word1) > i:
+        while i < len(word1) or i < len(word2):
+            if i < len(word1):
                 result += word1[i]
-            if len(word2) > i:
+            if i < len(word2):
                 result += word2[i]
-
             i += 1
-
         return result
-
 # get a result string
 # get a counter for while loop
 # while loop through until end or strs
