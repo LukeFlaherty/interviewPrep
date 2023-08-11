f9dcfda68f122fa17a4c3c3fab918d339b7440ff
Luke <flaherty.luke@yahoo.com>
2023-08-10 23:10:06 -0400

    simple

--- flowers.py
+++ flowers.py
@@ -4,23 +4,18 @@ class Solution:
         if n == 0: return True
 
         flowerbed = [0] + flowerbed + [0]
-
         for i in range(1, len(flowerbed)-1):
             if flowerbed[i] == 1:
                 continue
-            elif flowerbed[i-1] == flowerbed[i+1] == 0:
+            if flowerbed[i-1] == flowerbed[i+1] == 0:
                 n -= 1
                 flowerbed[i] = 1
-            
         return n <= 0
-            
-
 
 # If n is 0, then return true. 
 
 # We wrap 2 available plots to original flowerbed in case if the first plot is available. 
-        # bc if the first is available we cant check in front of it making us loop twice?
-
+       
 # We iterate the flowerbed. Minus the added plots
 
 # If the current plot is planted then we skip. 
