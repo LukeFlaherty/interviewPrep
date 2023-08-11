3143a18dfaa3fdc672b1fb63801ad90b473fd0c2
Luke <flaherty.luke@yahoo.com>
2023-08-10 23:15:59 -0400

    simple

--- reverseWords.py
+++ reverseWords.py
@@ -3,8 +3,4 @@ class Solution:
 
         return " ".join(s.split()[::-1])
 
-# reverse string by splitting sliced back to front
-
-# loop through new reversed string and append to stack
-
-# return stack joined with space as the splitter
+# return space joined by s split into list spliced back ot front
