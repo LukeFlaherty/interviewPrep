65c9c4578e72c53e904bb31c50ba19ccf448879f
Luke <flaherty.luke@yahoo.com>
2023-08-10 22:57:05 -0400

    remove line

--- mergeStringsAlternatively.py
+++ mergeStringsAlternatively.py
@@ -2,6 +2,7 @@ class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
         result = ""
         i = 0
+
         while i < len(word1) or i < len(word2):
             if i < len(word1):
                 result += word1[i]
